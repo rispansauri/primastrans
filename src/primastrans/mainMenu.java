8a0436c0d00186021b90dfd4f7ba93e9558845f4
Your Name <rispansaurry@gmail.com>
2021-04-11 03:20:59 +0700

    rstr

--- src/primastrans/mainMenu.java
+++ src/primastrans/mainMenu.java
@@ -752,44 +752,32 @@ public class mainMenu extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jLabel4MouseClicked
     
-    
+    String path=null;
     private void jLabel5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel5MouseClicked
         // TODO add your handling code here:
-//        try {
-//            /*NOTE: String s is the mysql file name including the .sql in its name*/
-//            /*NOTE: Getting path to the Jar file being executed*/
-//            /*NOTE: YourImplementingClass-> replace with the class executing the code*/
-//            CodeSource codeSource = mainMenu.class.getProtectionDomain().getCodeSource();
-//            File jarFile = new File(codeSource.getLocation().toURI().getPath());
-//            String jarDir = jarFile.getParentFile().getPath();
-//
-//            /*NOTE: Creating Database Constraints*/
-//             String dbName = "YourDBName";
-//             String dbUser = "YourUserName";
-//             String dbPass = "YourUserPassword";
-//
-//            /*NOTE: Creating Path Constraints for restoring*/
-//            String restorePath = jarDir + "\\backup" + "\\" + s;
-//
-//            /*NOTE: Used to create a cmd command*/
-//            /*NOTE: Do not create a single large string, this will cause buffer locking, use string array*/
-//            String[] executeCmd = new String[]{"mysql", dbName, "-u" + dbUser, "-p" + dbPass, "-e", " source " + restorePath};
-//
-//            /*NOTE: processComplete=0 if correctly executed, will contain other values if not*/
-//            Process runtimeProcess = Runtime.getRuntime().exec(executeCmd);
-//            int processComplete = runtimeProcess.waitFor();
-//
-//            /*NOTE: processComplete=0 if correctly executed, will contain other values if not*/
-//            if (processComplete == 0) {
-//                JOptionPane.showMessageDialog(null, "Successfully restored from SQL : " + s);
-//            } else {
-//                JOptionPane.showMessageDialog(null, "Error at restoring");
-//            }
-//
-//
-//        } catch (URISyntaxException | IOException | InterruptedException | HeadlessException ex) {
-//            JOptionPane.showMessageDialog(null, "Error at Restoredbfromsql" + ex.getMessage());
-//        }
+        JFileChooser fc = new JFileChooser();
+        fc.showOpenDialog(this);
+        try {
+            File f = fc.getSelectedFile();
+            path = f.getAbsolutePath();
+            path = path.replace('\\', '/');
+       
+            String dbUserName = "root";
+            String dbPassword = "";
+            String[] restoreCmd = new String[]{"C:/xampp/mysql/bin/mysqldump.exe ", "--user=" + dbUserName, "--password=" + dbPassword, "-e", "source " + path};
+            Process runtimProcess;
+        
+            runtimProcess = Runtime.getRuntime().exec(restoreCmd);
+            int proceCom = runtimProcess.waitFor();
+            
+            if (proceCom==0) {
+                JOptionPane.showMessageDialog(null, "Restored Succuss");
+            }else{
+                JOptionPane.showMessageDialog(null, "Can't Restored");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
     }//GEN-LAST:event_jLabel5MouseClicked
 
     private void jLabel6MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel6MouseClicked
